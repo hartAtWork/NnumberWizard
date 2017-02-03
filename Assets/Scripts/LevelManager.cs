07aee8f82dd7309cbe885f0c27dfda6aff2c8bee
hartAtWork <andrew.hart001@gmail.com>
2017-02-03 15:59:29 -0500

    Version 1

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel(string name){
+		Debug.Log("lvl reuqest "+name);
+		Application.LoadLevel(name);
+	}
+	public void Quit(){
+		Debug.Log("GoodBye");
+		Application.Quit();
+	}
+}
---
+++ Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class NumberWizard : MonoBehaviour {
+
+	// Use this for initialization
+	int max ;
+	int min;
+	int guess;
+	public int maxGuesses = 5;
+	
+	public Text text;
+	
+	
+	void Start () {
+		StartGame();
+	}
+	public void GuessHigher(){
+		min = guess;
+		NextGuess()	;
+	}
+	public void GuessLower(){
+		max = guess;
+		NextGuess()	;
+	}
+	void StartGame () {
+		max = 1000;
+		min = 1;
+		NextGuess();
+
+	}
+	void NextGuess(){
+		guess =  Random.Range (min, max+1) ;
+		text.text = guess.ToString();
+		maxGuesses = maxGuesses -1;
+		if(maxGuesses <= 0){
+			Application.LoadLevel("Win");
+		}
+	}
+}
